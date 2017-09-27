ebf582ae7d56f5297010b82d74dc13292a9b9d03
billcyz <billcyz@gmail.com>
2017-09-28 11:21:42 +1300

    twoSum problem

---
+++ erlBasement/src/leetCode/twoSum.erl
@@ -0,0 +1,16 @@
+%% @author billcyz
+%% @doc @todo Add description to twoSum.
+
+
+-module(twoSum).
+
+-export([a/0]).
+
+%% ------------------------------------------------------------------------
+
+%% leetCode twoSum solution
+-spec twoSum(list(), number()) -> list().
+twoSum(NumList, Target) ->
+	1.
+
+
