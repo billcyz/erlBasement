2dac33eeaf8d737f75e04095e98e76aec204466b
billcyz <billcyz@gmail.com>
2017-09-29 12:08:08 +1300

    two sum problem

--- erlBasement/src/leetCode/twoSum.erl
+++ erlBasement/src/leetCode/twoSum.erl
@@ -4,13 +4,25 @@
 
 -module(twoSum).
 
--export([a/0]).
+-export([twoSum/2]).
 
 %% ------------------------------------------------------------------------
 
 %% leetCode twoSum solution
+%% Return two list members that the sum of both member equals to target
 -spec twoSum(list(), number()) -> list().
 twoSum(NumList, Target) ->
-	1.
+	[H|T] = NumList,
+	L = [{X, Y} || X <- [H], 
+				   Y <- T, 
+				   X + Y == Target],
+	twoSum(T, L, Target).
 
+twoSum([], RList, _Target) -> RList;
+twoSum(NList, RList, Target) ->
+	[H|T] = NList,
+	L = [{X, Y} || X <- [H],
+				   Y <- T,
+				   X + Y == Target],
+	twoSum(T, RList ++ L, Target).
 
