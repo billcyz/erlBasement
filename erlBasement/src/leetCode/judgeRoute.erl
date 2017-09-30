8e9b70c9ef09eef67d784f21a2109bcb5a32fe57
billcyz <billcyz@gmail.com>
2017-10-01 10:49:22 +1300

    judge route solution

--- erlBasement/src/leetCode/judgeRoute.erl
+++ erlBasement/src/leetCode/judgeRoute.erl
@@ -4,14 +4,28 @@
 
 -module(judgeRoute).
 
--export([judge/1]).
+-export([judge_route/1, judge/3]).
 
 %% -----------------------------------------------------------------------
 
 %% input "UDURL"
--spec judge(list()) -> true | false.
-judge(RouteL) ->
-	[H|_] = RouteL,
-	H.
+judge_route(RouteL) ->
+	judge(RouteL, 0, 0).
+
+-spec judge(list(), integer(), integer()) -> true | false.
+judge([], ResultX, ResultY) ->
+	if
+		ResultX =:= 0, ResultY =:= 0 -> true;
+		true -> false
+	end;
+judge(RouteL, ResultX, ResultY) ->
+	[H|T] = RouteL,
+	if
+		H =:= 68 -> judge(T, ResultX, ResultY - 1); %% D
+		H =:= 85 -> judge(T, ResultX, ResultY + 1); %% U
+		H =:= 76 -> judge(T, ResultX - 1, ResultY); %% L
+		H =:= 82 -> judge(T, ResultX + 1, ResultY)  %% R
+	end.
+
 
 
